a5bde706dee6105c761dc071a430870fae3a95d6
Siddharth Pande <sidarjunstn.sp@gmail.com>
2026-02-22 20:57:51 +0530

    Tailwindcss added for styling

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,9 @@
+const Navigation = () => {
+  return (
+    <>
+      <h1 className="bg-red-500">Hello App is setup</h1>
+    </>
+  )
+}
+
+export default Navigation
---
+++ src/pages/App.jsx
@@ -0,0 +1,11 @@
+import Navigation from "../components/Navigation"
+
+const App = () => {
+  return (
+    <>
+      <Navigation />
+    </>
+  )
+}
+
+export default App
